4cd6dd84eef7bb98c1f3a7892274716d3dcdeeda
Kanokpron Thanawattanakun <kanokpron10@MacBook-Pro-khxng-Kanokpron.local>
2019-10-21 14:02:47 +0700

    [Edited]: แก้ไขเทส Test_GetIncomes_Input_MemberID_006_Year_2019_Month_12_Should_Be_Incomes_Day_11_And_12 ในไฟล์ internal/repository/repository_test.go

--- internal/repository/repository_test.go
+++ internal/repository/repository_test.go
@@ -135,11 +135,11 @@ func Test_GetMemberByID_Input_MemberID_001_Should_Be_Member(t *testing.T) {
 }
 
 func Test_GetIncomes_Input_MemberID_006_Year_2019_Month_12_Should_Be_Incomes_Day_11_And_12(t *testing.T) {
-	startTimeAM, _ := time.Parse("0000-01-01 15:04:05", "0000-01-01 09:00:00")
-	endTimeAM, _ := time.Parse("0000-01-01 15:04:05", "0000-01-01 12:00:00")
-	startTimePM, _ := time.Parse("0000-01-01 15:04:05", "0000-01-01 13:00:00")
-	endTimePM, _ := time.Parse("0000-01-01 15:04:05", "0000-01-01 18:00:00")
-	totalHours, _ := time.Parse("0000-01-01 15:04:05", "0000-01-01 08:00:00")
+	startTimeAM, _ := time.Parse("2018-01-01 15:04:05", "2018-12-01 09:00:00")
+	endTimeAM, _ := time.Parse("2018-01-01 15:04:05", "2018-12-01 12:00:00")
+	startTimePM, _ := time.Parse("2018-01-01 15:04:05", "2018-12-01 13:00:00")
+	endTimePM, _ := time.Parse("2018-01-01 15:04:05", "2018-12-01 18:00:00")
+	totalHours, _ := time.Parse("2018-01-01 15:04:05", "2018-12-01 08:00:00")
 	expected := []model.Incomes{
 		{
 			ID:                       58,
