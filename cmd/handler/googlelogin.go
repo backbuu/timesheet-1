d7ca1f81421c50e74c73d12ac3da2f4cb313d2f1
kanommoji <uracha_p@kkumail.com>
2019-11-06 12:02:39 +0700

    [Added]: สร้างฟังก์ชัน DeleteOauthStateCookie ใน cmd/handler/

--- cmd/handler/googlelogin.go
+++ cmd/handler/googlelogin.go
@@ -11,6 +11,7 @@ import (
 	"net/http"
 	"os"
 	"strings"
+
 	"time"
 	"timesheet/internal/model"
 
@@ -36,15 +37,21 @@ func OauthGoogleLogin(context *gin.Context) {
 }
 
 func (api TimesheetAPI) OauthGoogleLogout(context *gin.Context) {
-	requestToken := context.GetHeader("Authorization")
-	splitToken := strings.Split(requestToken, "Bearer ")
-	requestToken = splitToken[1]
-	err := api.TimesheetRepository.DeleteAuthentication(requestToken)
+	cookie, err := context.Request.Cookie("access_token")
+	splitToken := strings.Split(cookie.String(), "access_token=")
+	accessToken := splitToken[1]
+	err = api.TimesheetRepository.DeleteAuthentication(accessToken)
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
-	cookie := &http.Cookie{Name: "access_token", Value: "", Path: "/", Expires: time.Unix(0, 0)}
+	cookie = &http.Cookie{Name: "access_token", Value: "", Path: "/", Expires: time.Unix(0, 0), HttpOnly: true}
+	http.SetCookie(context.Writer, cookie)
+	context.Status(http.StatusOK)
+}
+
+func DeleteOauthStateCookie(context *gin.Context) {
+	cookie := &http.Cookie{Name: "oauthstate", Value: "", Path: "/", Expires: time.Unix(0, 0)}
 	http.SetCookie(context.Writer, cookie)
 	context.Status(http.StatusOK)
 }
@@ -77,7 +84,7 @@ func (api TimesheetAPI) OauthGoogleCallback(context *gin.Context) {
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 	}
-	cookie := http.Cookie{Name: "access_token", Value: "Bearer " + token.AccessToken, Expires: time.Now().Add(365 * 24 * time.Hour)}
+	cookie := http.Cookie{Name: "access_token", Value: token.AccessToken, Expires: time.Now().Add(365 * 24 * time.Hour)}
 	http.SetCookie(context.Writer, &cookie)
 	context.Redirect(http.StatusTemporaryRedirect, "/home")
 }
