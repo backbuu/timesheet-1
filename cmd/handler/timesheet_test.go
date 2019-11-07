92af0c70911b7e43155ee9f1c5a6e34a70f33565
kanommoji <uracha_p@kkumail.com>
2019-11-07 11:20:51 +0700

    [Edited]: แก้เทส set cookie ใน Test_GetProfileHandler_Input_AccessToken_Should_Be_Profile ใน cmd/handler/

--- cmd/handler/timesheet_test.go
+++ cmd/handler/timesheet_test.go
@@ -564,9 +564,8 @@ func Test_GetHolidayListHandler_Input_Month_1_Should_Be_HolidayList(t *testing.T
 func Test_GetProfileHandler_Input_AccessToken_Should_Be_Profile(t *testing.T) {
 	expected := `{"member_id":"007","email":"logintest535@gmail.com","picture":"https://lh4.googleusercontent.com/-nA86bkk5Icc/AAAAAAAAAAI/AAAAAAAAAAA/Wixwdu9UCfU/photo.jpg"}`
 	request := httptest.NewRequest("GET", "/showProfile", nil)
-	request.Header.Add("Authorization", "Bearer ya29.Il-vB2mB0hkAEN8KdupS3ZEaXBOHk6qhVntGSkeyAMz6KEoJOpwhfHHQF2KT9W2oiwE1op4pZiUuebKcQ1SBRgRlxMRJxB6Qjf0tl86C5Jdsf51thN-yqvZDBUmUx3hnqw")
 	writer := httptest.NewRecorder()
-
+	http.SetCookie(writer, &http.Cookie{Name: "access_token", Value: "ya29.Il-vB2mB0hkAEN8KdupS3ZEaXBOHk6qhVntGSkeyAMz6KEoJOpwhfHHQF2KT9W2oiwE1op4pZiUuebKcQ1SBRgRlxMRJxB6Qjf0tl86C5Jdsf51thN-yqvZDBUmUx3hnqw", Expires: time.Now().Add(365 * 24 * time.Hour)})
 	mockRepository := new(mockapi.MockRepository)
 	mockRepository.On("GetProfileByAccessToken", mock.Anything).Return(model.Profile{
 		MemberID: "007",
