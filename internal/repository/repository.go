2256f31e62442a251c21a2978d43e340f7b67aac
kanommoji <uracha_p@kkumail.com>
2019-11-11 17:22:34 +0700

    [Added]: เพิ่ม GetProfileByEmail ใน interface TimesheetRepositoryGateways ใน internal/repository/

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -22,6 +22,7 @@ type TimesheetRepositoryGateways interface {
 	DeleteIncome(incomeID int) error
 	UpdateMemberDetails(memberDetails model.Member) error
 	UpdatePictureToMembers(picture, email string) error
+	GetProfileByEmail(email string) (model.Profile, error)
 }
 
 type TimesheetRepositoryGatewaysToTimesheet interface {
