b97682440ed4aacc21927fde72c13ce19b691a80
kanommoji <uracha_p@kkumail.com>
2019-11-12 16:09:42 +0700

    [Edited]: แก้ไขใช้ id_token แทน access_token เทส Test_OauthGoogleLogout_Input_AccessToken_Should_Be_200 ใน cmd/handler/

--- cmd/handler/googlelogin_test.go
+++ cmd/handler/googlelogin_test.go
@@ -4,25 +4,19 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
-	"timesheet/cmd/mockapi"
 
 	. "timesheet/cmd/handler"
 
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
 )
 
 func Test_OauthGoogleLogout_Input_AccessToken_Should_Be_200(t *testing.T) {
 	request := httptest.NewRequest("POST", "/logout", nil)
 	writer := httptest.NewRecorder()
-	request.Header.Add("Authorization", "Bearer ya29.Il-vB2mB0hkAEN8KdupS3ZEaXBOHk6qhVntGSkeyAMz6KEoJOpwhfHHQF2KT9W2oiwE1op4pZiUuebKcQ1SBRgRlxMRJxB6Qjf0tl86C5Jdsf51thN-yqvZDBUmUx3hnqw")
-	mockRepository := new(mockapi.MockRepository)
-	mockRepository.On("DeleteAuthentication", mock.Anything).Return(nil)
+	request.Header.Add("Authorization", "eyJhbGciOiJSUzI1NiIsImtpZCI6ImEwNjgyNGI3OWUzOTgyMzk0ZDVjZTdhYzc1YmY5MmNiYTMwYTJlMjUiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiI2OTI1NzU4OTgzOTctZG50OXNxaTJqc3RkZGZlcHNuZzA0cDlhYzRvajdwNG4uYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiI2OTI1NzU4OTgzOTctZG50OXNxaTJqc3RkZGZlcHNuZzA0cDlhYzRvajdwNG4uYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMTAzMDYxODkyODYyMDM5OTgxMzIiLCJlbWFpbCI6ImxvZ2ludGVzdDUzNUBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiYXRfaGFzaCI6InRlWmZfdnZoVTQxQXBqTWdxbGFvX1EiLCJpYXQiOjE1NzM0NjIxNzQsImV4cCI6MTU3MzQ2NTc3NH0.FieIq3nqnEk4sKgNN3gOAHRat-Gj7ewvLV6ri9P4k1_PsoBOSL2brb02HAYrYFYl1NPFwymcp96j_5ZbZnV2k2JbhXvaocPc75pUO8pfzNzVzSp8JiU-OpqUb5CSoguJ6ejLTTGLzFkZ2Uu51GY0Kb_SNkSMGXHwIOlIdSx2UzqrfAqZAliSp_5D1Cp7Ot1I95uv0C79h3TB0ODY9zESsP4lF542ic9sseCt7KCfmoh9hq24OBW9nRLOPqXhOgInvvtqghQd2p7nv88GUdMuCOAFJZgg3_5zoLPkGBiAJcdwwcCoU-kd6r6mcxjKN2xbwFa4G5NskLzNRpUlJQpSRA")
 
-	api := TimesheetAPI{
-		Repository: mockRepository,
-	}
+	api := TimesheetAPI{}
 	testRoute := gin.Default()
 	testRoute.POST("/logout", api.OauthGoogleLogout)
 	testRoute.ServeHTTP(writer, request)
