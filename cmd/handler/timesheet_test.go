b4c3d095f2a87f1423bd098e9bf7ee29e9cc0906
kanommoji <uracha_p@kkumail.com>
2019-11-13 22:31:02 +0700

    [Edited]: แก้ไขชื่อ path จาก addIncomeItem เป็น createIncome ใน cmd/

--- cmd/handler/timesheet_test.go
+++ cmd/handler/timesheet_test.go
@@ -201,7 +201,7 @@ func Test_CreateIncomeHandler_Input_Year_2018_Month_12_EmployeeID_001_Income_Sho
 		},
 	}
 	jsonRequest, _ := json.Marshal(requestIncome)
-	request := httptest.NewRequest("POST", "/addIncomeItem", bytes.NewBuffer(jsonRequest))
+	request := httptest.NewRequest("POST", "/createIncome", bytes.NewBuffer(jsonRequest))
 	request.Header.Add("Authorization", "eyJhbGciOiJSUzI1NiIsImtpZCI6ImEwNjgyNGI3OWUzOTgyMzk0ZDVjZTdhYzc1YmY5MmNiYTMwYTJlMjUiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiI2OTI1NzU4OTgzOTctZG50OXNxaTJqc3RkZGZlcHNuZzA0cDlhYzRvajdwNG4uYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiI2OTI1NzU4OTgzOTctZG50OXNxaTJqc3RkZGZlcHNuZzA0cDlhYzRvajdwNG4uYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMTAzMDYxODkyODYyMDM5OTgxMzIiLCJlbWFpbCI6ImxvZ2ludGVzdDUzNUBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiYXRfaGFzaCI6InRlWmZfdnZoVTQxQXBqTWdxbGFvX1EiLCJpYXQiOjE1NzM0NjIxNzQsImV4cCI6MTU3MzQ2NTc3NH0.FieIq3nqnEk4sKgNN3gOAHRat-Gj7ewvLV6ri9P4k1_PsoBOSL2brb02HAYrYFYl1NPFwymcp96j_5ZbZnV2k2JbhXvaocPc75pUO8pfzNzVzSp8JiU-OpqUb5CSoguJ6ejLTTGLzFkZ2Uu51GY0Kb_SNkSMGXHwIOlIdSx2UzqrfAqZAliSp_5D1Cp7Ot1I95uv0C79h3TB0ODY9zESsP4lF542ic9sseCt7KCfmoh9hq24OBW9nRLOPqXhOgInvvtqghQd2p7nv88GUdMuCOAFJZgg3_5zoLPkGBiAJcdwwcCoU-kd6r6mcxjKN2xbwFa4G5NskLzNRpUlJQpSRA")
 	writer := httptest.NewRecorder()
 
@@ -228,7 +228,7 @@ func Test_CreateIncomeHandler_Input_Year_2018_Month_12_EmployeeID_001_Income_Sho
 		Repository: mockRepository,
 	}
 	testRoute := gin.Default()
-	testRoute.POST("/addIncomeItem", api.CreateIncomeHandler)
+	testRoute.POST("/createIncome", api.CreateIncomeHandler)
 	testRoute.ServeHTTP(writer, request)
 	response := writer.Result()
 
