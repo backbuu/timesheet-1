7a4704f3502dfbdac52aa73dffe811629d4a490a
kanommoji <uracha_p@kkumail.com>
2019-11-17 21:07:46 +0700

    [Edited]: แก้ไขการเทสไปใช้ assert.Equal เทส Test_GetURI_Tnput_DatabaseConfig_Username_root_Password_root_Host_my_mariadb_Port_3306_Database_timesheet_Should_Be_URI ใน config/

--- config/config_test.go
+++ config/config_test.go
@@ -3,10 +3,12 @@ package config_test
 import (
 	"testing"
 	"timesheet/config"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_GetURI_Tnput_DatabaseConfig_Username_root_Password_root_Host_my_mariadb_Port_3306_Database_timesheet_Should_Be_URI(t *testing.T) {
-	expectedResult := "root:root@tcp(my-mariadb:3306)/timesheet?parseTime=true"
+	expected := "root:root@tcp(my-mariadb:3306)/timesheet?parseTime=true"
 	databaseConfig := config.DatabaseConfig{
 		Username: "root",
 		Password: "root",
@@ -15,9 +17,7 @@ func Test_GetURI_Tnput_DatabaseConfig_Username_root_Password_root_Host_my_mariad
 		Database: "timesheet",
 	}
 
-	actualResult := databaseConfig.GetURI()
+	actual := databaseConfig.GetURI()
 
-	if expectedResult != actualResult {
-		t.Errorf("Expect %v but get %v", expectedResult, actualResult)
-	}
+	assert.Equal(t, expected, actual)
 }
